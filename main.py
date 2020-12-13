1e6d8a8c507966a341d77ee07bb17a4abcb0f6e6
haitao <yueht17@gmail.com>
2020-12-13 14:06:12 +0800

    添加了main

---
+++ main.py
@@ -0,0 +1,20 @@
+#! usr/bin/env python
+# -*- coding:utf-8 -*-
+from Network import Network
+from time import time
+from Saver import Saver
+
+if __name__ == "__main__":
+
+    net = Network()
+    saver = Saver()
+    net.Monte_Carlo_Times = 30000
+    intervals = [_ / 365 for _ in range(1, 180)]
+    for interval in intervals:
+        saver.info_log("Starting simulation, interval" + str(interval))
+        begin = time()
+        res = net.output(parallel=True, interval=interval)
+        res["time_spend"] = time() - begin
+        print("output", res)
+        saver.info_log("End simulation")
+        saver.add_result(res)
